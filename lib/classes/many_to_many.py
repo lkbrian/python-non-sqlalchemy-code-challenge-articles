0d0b6fba32e50b78bf273cb2384e4b9306849383
lkbrian <lkbrian742@gmail.com>
2024-03-10 21:19:38 +0300

    Completed Assignment

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,108 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
+        if not isinstance(title, str) and not 5 <= len(title) <= 50:
+            raise Exception("the string should be more than 1 letter")
+        self.title = title
         self.author = author
         self.magazine = magazine
-        self.title = title
-        
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str) or not 5 <= len(value) <= 50:
+            raise Exception("the string should be more than 1 letter")
+        self._title = value
+
+
 class Author:
     def __init__(self, name):
+        if not isinstance(name, str) and not (len(name) > 0):
+            raise Exception("invalid")
         self.name = name
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (len(value) > 0):
+            raise Exception("invalid")
+        self._name = value
+
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set(article.magazine for article in self.articles()))
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        self.articles().append(article)
+        return article
 
     def topic_areas(self):
-        pass
+        return (
+            list(set(magazine.category for magazine in self.magazines()))
+            if self.articles()
+            else None
+        )
+
 
 class Magazine:
+    all = []
+
     def __init__(self, name, category):
+        if not isinstance(name, str) or not (2 <= len(name) <= 16):
+            raise Exception("Invalid name length between 2 and 16 characters")
         self.name = name
+
+        if not isinstance(category, str) or len(category) == 0:
+            raise Exception("Not a valid category")
         self.category = category
 
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not 2 <= len(value) <= 16:
+            raise Exception("Invalid name length between 2 and 16 characters")
+        self._name = value
+
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in self.articles()))
 
     def article_titles(self):
-        pass
+        return [article.title for article in self.articles()] if self.articles() else None
 
     def contributing_authors(self):
-        pass
+        authors = {}
+        for article in self.articles():
+            author = article.author
+            if author in authors:
+                authors[author] += 1
+            else:
+                authors[author] = 1
+        
+        return [author for author, count in authors.items() if count > 2 and isinstance(author, Author)]
+
+
+    # @classmethod
+    # def top_publisher(cls):
+    #     if not cls.all:
+    #         return None
+    #     return max(cls.all, key=lambda magazine: len(magazine.articles()))
+
--- lib/debug.py
+++ lib/debug.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
-import ipdb;
+import ipdb
 
 from classes.many_to_many import Article
 from classes.many_to_many import Author
 from classes.many_to_many import Magazine
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("HELLO! :) let's debug :vibing_potato:")
-
 
     # don't remove this line, it's for debugging!
     ipdb.set_trace()
+    author1 = Author("John Doe")
+    magazine1 = Magazine("Tech Mag", "Technology")
+    ipdb.set_trace()
+    article1 = author1.add_article(magazine1, "Python Programming")
+    print(author1.name)
+    print(author1.articles())
+    print(author1.magazines())
